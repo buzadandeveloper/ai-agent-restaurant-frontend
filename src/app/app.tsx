e8e31d4058d763ba8e6980f026db211712ffdf4f
Buza Dan <buzadandeveloper@gmail.com>
2026-01-04 20:57:17 +0200

    feat: adjustments

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,8 +1,5 @@
 function App() {
-    return (
-        <div className='w-full flex h-[1000px] justify-center items-center'>
-        </div>
-    );
+  return <div className="w-full flex h-[1000px] justify-center items-center"></div>;
 }
 
 export default App;
--- src/lib/show-toast.tsx
+++ src/lib/show-toast.tsx
@@ -1,12 +1,12 @@
-import {toast} from 'sonner';
-import {Toast} from "@/components/common/toast/toast";
+import { toast } from "sonner";
+import { Toast } from "@/components/common/toast/toast";
 
 interface Props {
-    title: string;
-    description: string;
-    variant: 'default' | 'destructive';
-};
+  title: string;
+  description: string;
+  variant: "default" | "destructive";
+}
 
-export function showToast({title, description, variant}: Props) {
-    toast.custom(() => <Toast title={title} description={description} variant={variant}/>);
+export function showToast({ title, description, variant }: Props) {
+  toast.custom(() => <Toast title={title} description={description} variant={variant} />);
 }
