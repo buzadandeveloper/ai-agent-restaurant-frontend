b359adfa939cff6c0ad8221fbe3cdcbef1eba275
Buza Dan <buzadandeveloper@gmail.com>
2026-01-07 19:24:12 +0200

    feat: integrate axios conifg

--- src/config/response-interceptor.config.ts
+++ src/config/response-interceptor.config.ts
@@ -1,10 +1,13 @@
 import type { AxiosError, AxiosResponse } from "axios";
+import {showToast} from "@/lib/show-toast";
 
 const responseInterceptor = (response: AxiosResponse): AxiosResponse => {
   return response;
 };
 
 const responseErrorInterceptor = async (error: AxiosError) => {
+  showToast({ title: "Error", description: "Something went wrong.", variant: "destructive" });
+
   return Promise.reject(error);
 };
 
