44e083c6aa0504c71c8d8ed8052c59da1e125a1b
Buza Dan <buzadandeveloper@gmail.com>
2026-03-11 20:59:38 +0200

    feat: add services for reset password

--- src/services/auth/auth-service.ts
+++ src/services/auth/auth-service.ts
@@ -6,6 +6,20 @@ class AuthService {
 
   login = (payload: LoginData): Promise<{ accessToken: string }> =>
     request.post("/api/auth/login", payload);
+
+  forgotPassword = (payload: { email: string }): Promise<{ message: string }> =>
+    request.post("/api/auth/forgot-password", payload);
+
+  verifyResetCode = (payload: {
+    email: string;
+    code: string;
+  }): Promise<{ message: string; resetToken: string }> =>
+    request.post("/api/auth/verify-reset-code", payload);
+
+  resetPassword = (payload: {
+    resetCode: string;
+    newPassword: string;
+  }): Promise<{ message: string }> => request.post("/api/auth/reset-password", payload);
 }
 
 export const authService = new AuthService();
